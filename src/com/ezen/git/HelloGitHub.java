89eb8188b1095177dc8f6e8b3756e8832b612e3d
rudcks8389 <rudcks8389@naver.com>
2024-04-04 16:31:51 +0900

    민경찬 기능추가

--- src/com/ezen/git/HelloGitHub.java
+++ src/com/ezen/git/HelloGitHub.java
@@ -3,5 +3,6 @@ package com.ezen.git;
 public class HelloGitHub {
     public static void main(String[] args) {
         System.out.println("111111111");
+        System.out.println("222222");
     }
 }
